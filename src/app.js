10620477866eb175071fc5d47af2af71cc6e22bf
jhgoni <106790228+jhgoni@users.noreply.github.com>
2023-07-11 17:13:21 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#buttonExcuse").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird",
+    "A racoon",
+    "A jogger"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "stole", "threw"];
+  let what = ["my homework", "the keys", "the car", "my shoe"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
